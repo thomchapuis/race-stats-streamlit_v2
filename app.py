50d5a19be84085ba220c1f2c47483897c5ef8fce
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-14 16:34:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import streamlit as st
+
+@st.cache_data
+def load_data():
+    return pd.read_parquet("data/races5.parquet")
+
+df_all_parquet = load_data()
+st.write(df_all_parquet.head())
 
