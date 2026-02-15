087a0f162effe7c4baa381de466ce2250be4c17a
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-15 17:28:07 +0100

    Update fonctions.py

--- utils/fonctions.py
+++ utils/fonctions.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import math
+import numpy as np
 
 # ---------------------------------------------------------------------------------------
 # 🔹 Fonction de Viz
