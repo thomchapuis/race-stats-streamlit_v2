6fcd16d0c0f3b6a4ae76b3a13231622ead627ca4
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-15 10:20:46 +0100

    Add imports for Plotly and Unicode handling
    Added new imports for data visualization and processing.

--- utils/fonctions.py
+++ utils/fonctions.py
@@ -1,6 +1,10 @@
 import pandas as pd
-import re
-
+import unicodedata
+import os
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import math
 
 # ---------------------------------------------------------------------------------------
 # 🔹 Fonction de Viz
