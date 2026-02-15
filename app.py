4b9c1859655830ba90adb57d8bb03078f927203a
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-15 09:41:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 data_file = "data/races5.parquet"
 
 st.set_page_config(layout="wide")
+
 @st.cache_data
 def load_data():
     return pd.read_parquet(data_file)
@@ -48,12 +49,9 @@ with tab3:
     
     # Affichage en cartes (colonnes dynamiques)
     cols = st.columns(len(courses_par_sport))
-    
     for col, (sport, nb) in zip(cols, courses_par_sport.items()):
         with col:
-            st.metric(
-                label=sport,
-                value=f"{nb:,}".replace(",", " "),
-            )
-
+            with st.container(border=True):
+                st.markdown(f"**{sport}**")
+                st.markdown(f"### {nb}")
 ########################## ########################## ########################## ########################## ########################## 
