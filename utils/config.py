4c893fa0aa94ea0795a1667a2fd971f2108fb210
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-15 09:55:28 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,10 @@
+SPORT_ICONS = {
+    "Trail": "🏔️",
+    "Running": "🏃‍♂️",
+    "Cyclisme": "🚴‍♂️",
+    "Triathlon": "🏊‍♂️🚴‍♂️🏃‍♂️"
+}
 
+def sport_icon(sport):
+    """Retourne l'icône correspondant au sport ou un drapeau par défaut."""
+    return SPORT_ICONS.get(sport, "🏁")
