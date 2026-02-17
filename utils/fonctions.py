8cbb7aae269012139194bde75db6604d429b0b63
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-17 06:46:50 +0100

    Update fonctions.py

--- utils/fonctions.py
+++ utils/fonctions.py
@@ -82,8 +82,8 @@ def Viz_Sexes_PieChart(df_single_race):
         names='Genre',
         title=f"Répartition Hommes/Femmes - {df_single_race['race_name'].iloc[0]} ({df_single_race['race_date'].dt.year.iloc[0]})",
         color=['Genre'],
-        color_discrete_map={'Femmes': '#e84393', 'Hommes': '#3498db'},
-        hole=0.3  # Optionnel : pour un donut chart
+        color_discrete_map={'Femmes': '#e84393', 'Hommes': '#3498db'}
+        #hole=0.3  # Optionnel : pour un donut chart
     )
 
     # Personnalisation de la mise en page
