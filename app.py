51d4c99ff3c26910ad3f9895cfbdc182cc23cea2
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-18 16:17:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ df_all_parquet = pd.merge(
 )
 
 #df_all_parquet["race_key"] = (df_all_parquet["race_name"].astype(str)+ " - " + df_all_parquet["race_date"].astype(str).str[:4]+ " - "+ df_all_parquet["Distance"].round().astype("Int64").astype(str)+ "km")
-df_all_parquet["race_key"] = (df_all_parquet["race_name"].astype(str)+ " - " + df_all_parquet["race_date"].astype(str).str[:4]+ " - "+ df_all_parquet["Distance"].astype(str)+ "km")
+#df_all_parquet["race_key"] = (df_all_parquet["race_name"].astype(str)+ " - " + df_all_parquet["race_date"].astype(str).str[:4]+ " - "+ df_all_parquet["Distance"].astype(str)+ "km")
 df_all_parquet["race_key"] = (df_all_parquet["race_name"].astype(str)+ " - " + df_all_parquet["race_date"].astype(str).str[:4]+ " - "+ df_all_parquet["Distance"].astype(str)+ "km")
 
 # ---------------------------------------------------------------------------------
@@ -84,7 +84,7 @@ with tab2:
 
     
     #st.write(int(df_all_parquet.loc[df_all_parquet["race_key"] == race_recherche_v2, "race_date"].astype(str).str[:4].iloc[0]))
-    #st.write(int(df_all_parquet.loc[df_all_parquet["race_key"] == race_recherche_v2, "Distance"].iloc[0]))
+    st.write(int(df_all_parquet.loc[df_all_parquet["race_key"] == race_recherche_v2, "Distance"].iloc[0]))
 
     
     if not race_recherche_v1:
