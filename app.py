e575377cc6e5de798e89ad0a735d70f5c6b61b6c
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-02-24 10:04:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ with tab6:
     st.write(df_synthese)
     #st.write(df_all_parquet.head())
     st.write(
-        df_all_parquet[["Race", "race_name", "race_key", "Distance"]]
+        df_all_parquet[["Race1", "race_name", "race_key", "Distance"]]
         .drop_duplicates(subset="race_key")
     )
 
